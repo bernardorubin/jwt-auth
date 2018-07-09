349fac8e606c88d9a48de6840456b696d3260906
Bernardo Emilio Rubin de Celis Martinez <bernulander@gmail.com>
2018-07-08 19:59:08 -0500

    Provide users with a JSON web token

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 const PORT = process.env.PORT || 8888;
+//USER DB ->
+// always encrypt passwords in production
+const users = [
+  {id: 1, username: "admin", password: "admin"},
+  {id: 2, username: "guest", password: "guest"},
+];
 
 app.use(bodyParser.json());
 
 app.post('/login', (req, res) => {
-  const user = req.body.username;
+  //check if request formatted correctly
+  if(!req.body.username || !req.body.password) {
+    res
+    .status(400)
+    .send("You need a username and password");
+    return;
+  }
+  // check if we have a username that matches
+  const user = users.find((u) => {
+    return u.username === req.body.username && u.password === req.body.password;
+  });
+  if(!user) {
+    res
+    .status(401)
+    .send('User not found');
+    return;
+  }
+  // use sign method to create sign token
+  const token = jwt.sign({
+    sub: user.id,
+    username: user.username
+  }, "mysupersecretkey", {expiresIn: "3 hours"});
 
   res
   .status(200)
-  .send(`You logged in with ${user}`);
+  .send({access_token: token});
 });
 
 app.get('/status', (req, res) => {
